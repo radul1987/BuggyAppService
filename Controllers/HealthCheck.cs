7d4ec873e962d894e1b0dd7bd63e24d868544d0e
Radu Tanasescu <ratanas@microsoft.com>
2023-10-11 14:25:16 +0300

    induced error on health check page

--- Controllers/HealthCheck.cs
+++ Controllers/HealthCheck.cs
@@ -6,9 +6,14 @@ namespace BuggyAppService.Controllers
 {
     public class HealthCheck : Controller
     {
-
+        static int numberOfFailures = 0;
         public IActionResult Index()
         {
+
+            if (numberOfFailures < 10) {
+                numberOfFailures++;
+                throw new Exception("induced 10 errors for health check feature when app is starting");
+            }
           
             return View();
         }
