6ba41cae95d6a9c0752490dba04a6677e13e0858
Radu Tanasescu <ratanas@microsoft.com>
2023-10-11 14:25:16 +0300

    cold start key using number of seconds to pass the  value

--- Program.cs
+++ Program.cs
@@ -18,18 +18,23 @@ if (!app.Environment.IsDevelopment())
 string startupIssue = "false";
 try
 {
-     startupIssue = System.Environment.GetEnvironmentVariables()["StartupIssue"].ToString();
+    startupIssue = System.Environment.GetEnvironmentVariables()["StartupIssue"].ToString();
 
 }
-catch(Exception ex)
+catch (Exception ex)
 {
-   
+
 }
 // startupIssue = app.Configuration.GetValue<string>(("APPSETTING_StartupIssue"));
 
-if (startupIssue == "true")
+if (startupIssue.ToLower() == "true")
 {
     throw new Exception("startup issue");
+}
+
+else if (int.TryParse(startupIssue, out int coldsStartValue))
+{
+    System.Threading.Thread.Sleep(coldsStartValue * 1000);
 }
 else if (startupIssue == "coldStart60")
 {
