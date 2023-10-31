1e5b5dbb4e810c43a940d6faf527291528365ef3
Radu Tanasescu <ratanas@microsoft.com>
2023-10-31 16:17:21 +0200

    Cient cert authentication 500page

--- Controllers/500Exception.cs
+++ Controllers/500Exception.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace BuggyAppService.Controllers
 {
@@ -6,6 +9,50 @@ namespace BuggyAppService.Controllers
     {
         public IActionResult Index()
         {
+
+            string host = @"https://clicertmap.neo.lan/";
+            string certName = @"C:\temp\User1.pfx";
+            string password = @"1234";
+
+            try
+            {
+                X509Certificate2Collection certificates = new X509Certificate2Collection();
+                certificates.Import(certName, password, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+
+                ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) => true;
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(host);
+                req.AllowAutoRedirect = true;
+                req.ClientCertificates = certificates;
+                req.Method = "GET";
+                //req.ContentType = "application/x-www-form-urlencoded";
+                //string postData = "login-form-type=cert";
+                //byte[] postBytes = Encoding.UTF8.GetBytes(postData);
+                //req.ContentLength = postBytes.Length;
+
+                //Stream postStream = req.GetRequestStream();
+                //postStream.Write(postBytes, 0, postBytes.Length);
+                //postStream.Flush();
+                //postStream.Close();
+                WebResponse resp = req.GetResponse();
+                string line = "";
+                Stream stream = resp.GetResponseStream();
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        line += reader.ReadLine();
+                    }
+                }
+
+                stream.Close();
+                ViewData["clientCert"] = line;
+            }
+            catch (Exception e) {
+                ViewData["clientCert"] = e.Message;
+            }
+         
             return View();
         }
 
