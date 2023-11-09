d86037eb76dcad6df0f248a6f8d07d2aa3246f5f
Dan <damatei@microsoft.com>
2023-11-09 14:47:58 +0200

    clicertauth 2

--- Controllers/ClientCertAuth.cs
+++ Controllers/ClientCertAuth.cs
@@ -18,25 +18,23 @@ namespace BuggyAppService.Controllers
         [HttpPost]
         public IActionResult Index(IFormFile postedFile)
         {
-            if (postedFile == null || postedFile.Length == 0)
-                return BadRequest("No file selected for upload...");
-
-            string fileName = Path.GetFileName(postedFile.FileName);
-            string contentType = postedFile.ContentType;
-
-            string host = Request.Form["url"];
-
-            string password = @"1234";
-
-
-            using (var stream = new MemoryStream())
+            try
             {
-                postedFile.CopyTo(stream);
-                byte[] bytes = stream.ToArray();
-                X509Certificate2Collection certificates = new X509Certificate2Collection();
-                certificates.Import(bytes, password, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
-                try
+                if (postedFile == null || postedFile.Length == 0)
+                    return BadRequest("No file selected for upload...");
+
+                string fileName = Path.GetFileName(postedFile.FileName);
+                string contentType = postedFile.ContentType;
+                string host = Request.Form["url"];
+                string password = Request.Form["certpass"];
+
+                using (var stream = new MemoryStream())
                 {
+                    postedFile.CopyTo(stream);
+                    byte[] bytes = stream.ToArray();
+                    X509Certificate2Collection certificates = new X509Certificate2Collection();
+                    certificates.Import(bytes, password, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+
                     ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) => true;
                     HttpWebRequest req = (HttpWebRequest)WebRequest.Create(host);
                     req.AllowAutoRedirect = true;
@@ -45,30 +43,30 @@ namespace BuggyAppService.Controllers
 
                     WebResponse resp = req.GetResponse();
                     HttpWebResponse RespStatusCode = (HttpWebResponse)resp;
-                    var x = (int)RespStatusCode.StatusCode; 
+                    var x = (int)RespStatusCode.StatusCode;
 
-//\                   string line = "";
-//                    Stream respStream = resp.GetResponseStream();
-//                    using (StreamReader reader = new StreamReader(respStream))
-//                    {
-//                        line = reader.ReadLine();
-//                        while (line != null)
-//                        {
-//                            Console.WriteLine(line);
-//                            line += reader.ReadLine();
-//                           //break;
-//                        }
-//                        respStream.Close();
-//                        ViewData["RequestStatus"] = line;
-//                    }
+                    //\                   string line = "";
+                    //                    Stream respStream = resp.GetResponseStream();
+                    //                    using (StreamReader reader = new StreamReader(respStream))
+                    //                    {
+                    //                        line = reader.ReadLine();
+                    //                        while (line != null)
+                    //                        {
+                    //                            Console.WriteLine(line);
+                    //                            line += reader.ReadLine();
+                    //                           //break;
+                    //                        }
+                    //                        respStream.Close();
+                    //                        ViewData["RequestStatus"] = line;
+                    //                    }
                     ViewData["RequestStatus"] = x;
                 }
-                catch (Exception e)
-                {
-                    ViewData["RequestStatus"] = e.Message;
-                }
-                return View();
             }
+            catch (Exception e)
+            {
+                ViewData["RequestStatus"] = e.Message;
+            }
+            return View();
         }
     }
 }
