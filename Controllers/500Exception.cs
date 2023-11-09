7c58ec0b00f7dd4b98c9f6275abc374cbf2cb0a3
Dan <damatei@microsoft.com>
2023-11-09 14:19:24 +0200

    certificate auth

--- Controllers/500Exception.cs
+++ Controllers/500Exception.cs
@@ -8,52 +8,7 @@ namespace BuggyAppService.Controllers
     public class _500Exception : Controller
     {
         public IActionResult Index()
-        {
-
-            string host = @"https://clicertmap.neo.lan/";
-            string certName = @"C:\temp\User1.pfx";
-            string password = @"1234";
-
-            try
-            {
-                X509Certificate2Collection certificates = new X509Certificate2Collection();
-                certificates.Import(certName, password, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
-
-                ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) => true;
-                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(host);
-                req.AllowAutoRedirect = true;
-                req.ClientCertificates = certificates;
-                req.Method = "GET";
-                //req.ContentType = "application/x-www-form-urlencoded";
-                //string postData = "login-form-type=cert";
-                //byte[] postBytes = Encoding.UTF8.GetBytes(postData);
-                //req.ContentLength = postBytes.Length;
-
-                //Stream postStream = req.GetRequestStream();
-                //postStream.Write(postBytes, 0, postBytes.Length);
-                //postStream.Flush();
-                //postStream.Close();
-                WebResponse resp = req.GetResponse();
-                string line = "";
-                Stream stream = resp.GetResponseStream();
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    line = reader.ReadLine();
-                    while (line != null)
-                    {
-                        Console.WriteLine(line);
-                        line += reader.ReadLine();
-                        break;
-                    }
-                }
-
-                stream.Close();
-                ViewData["clientCert"] = line;
-            }
-            catch (Exception e) {
-                ViewData["clientCert"] = e.Message;
-            }
-         
+        {     
             return View();
         }
 
---
+++ Controllers/ClientCertAuth.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace BuggyAppService.Controllers
+{
+    public class ClientCertAuth : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(IFormFile postedFile)
+        {
+            if (postedFile == null || postedFile.Length == 0)
+                return BadRequest("No file selected for upload...");
+
+            string fileName = Path.GetFileName(postedFile.FileName);
+            string contentType = postedFile.ContentType;
+
+            string host = Request.Form["url"];
+
+            string password = @"1234";
+
+
+            using (var stream = new MemoryStream())
+            {
+                postedFile.CopyTo(stream);
+                byte[] bytes = stream.ToArray();
+                X509Certificate2Collection certificates = new X509Certificate2Collection();
+                certificates.Import(bytes, password, X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+                try
+                {
+                    ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) => true;
+                    HttpWebRequest req = (HttpWebRequest)WebRequest.Create(host);
+                    req.AllowAutoRedirect = true;
+                    req.ClientCertificates = certificates;
+                    req.Method = "GET";
+
+                    WebResponse resp = req.GetResponse();
+                    HttpWebResponse RespStatusCode = (HttpWebResponse)resp;
+                    var x = (int)RespStatusCode.StatusCode; 
+
+//\                   string line = "";
+//                    Stream respStream = resp.GetResponseStream();
+//                    using (StreamReader reader = new StreamReader(respStream))
+//                    {
+//                        line = reader.ReadLine();
+//                        while (line != null)
+//                        {
+//                            Console.WriteLine(line);
+//                            line += reader.ReadLine();
+//                           //break;
+//                        }
+//                        respStream.Close();
+//                        ViewData["RequestStatus"] = line;
+//                    }
+                    ViewData["RequestStatus"] = x;
+                }
+                catch (Exception e)
+                {
+                    ViewData["RequestStatus"] = e.Message;
+                }
+                return View();
+            }
+        }
+    }
+}
